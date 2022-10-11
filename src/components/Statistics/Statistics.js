55b4b81fc653cc40710efecad391616988305064
Saima <saimasawrin324@gmail.com>
2022-10-12 00:31:24 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,61 +4,33 @@ import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, X
 
 
 const Statistics = () => {
-    const data = [
-        {
-          name: 'Page A',
-          marks: 4000,
-          quiz: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          marks: 3000,
-          quiz: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          marks: 2000,
-          quiz: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          marks: 2780,
-          quiz: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          marks: 1890,
-          quiz: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          marks: 2390,
-          quiz: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          marks: 3490,
-          quiz: 4300,
-          amt: 2100,
-        },
-      ];
+    const data = 
+      [{
+        name:"React",
+        total:8},
+      {name:"JavaScript",
+      total:9},
+      {
+        name:"CSS",
+        total:8},
+      {name:"Git",
+      total:11
+    },
+  ];
+
+       
+     
       
-    return (
-        <div>
-         <LineChart width={1000} height={400} data={data}>
+return (
+        <div className='chart'>
+         <LineChart width={800} height={400} data={data}>
             <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip/>
           <Legend />
-          <Line type="monotone" dataKey="quiz" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="marks" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
           </LineChart>
            
 
